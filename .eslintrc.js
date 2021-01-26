00fc8b2ed37f066d8203a235991a281d493402aa
Rubyc <759337828@qq.com>
2021-01-26 20:41:05 +0800

    1.项目目录规划 2.字体,公共样式 3.配置项目路径别名 4.stylus相关package

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
+    'arrow-parens': 'off',
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'eol-last': 'off',
+    'space-before-function-paren': 'off'
   }
 }
