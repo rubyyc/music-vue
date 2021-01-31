ffaaa6a25857c7c5028b886ad8f492ec0a714e5b
Rubyc <759337828@qq.com>
2021-01-31 20:33:24 +0800

    music-list渲染歌曲列表

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,15 +4,26 @@
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter">
       </div>
     </div>
+    <scroll :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import songList from 'base/song-list/song-list.vue'
+import Scroll from 'base/scroll/scroll.vue'
 export default {
+  components: {
+    songList,
+    Scroll
+  },
   props: {
     bgImage: {
       type: String,
@@ -26,6 +37,9 @@ export default {
       type: String,
       default: ''
     }
+  },
+  mounted() {
+    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
   },
   computed: {
     bgStyle() {
@@ -113,6 +127,7 @@ export default {
     .list
       position: fixed
       top: 0
+      z-index: -100 
       bottom: 0
       width: 100%
       background: $color-background
